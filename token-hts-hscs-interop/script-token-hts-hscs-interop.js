5f1eb98db4c147feac87eee0af938d36e00f622f
Brendan Graetz <bguiz@users.noreply.github.com>
2024-08-05 13:01:33 +0800

    proj: scaffold new task for option 3: tokens on HTS and HSCS interoperability
    Signed-off-by: Brendan Graetz <bguiz@users.noreply.github.com>

---
+++ token-hts-hscs-interop/script-token-hts-hscs-interop.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+import {
+    Client,
+    PrivateKey,
+    AccountId,
+} from '@hashgraph/sdk';
+import dotenv from 'dotenv';
+import {
+    createLogger,
+} from '../util/util.js';
+
+const logger = await createLogger({
+    scriptId: 'token-hts-hscs-interop',
+    scriptCategory: 'task',
+});
+let client;
+
+async function scriptToken-hts-hscs-interop() {
+    logger.logStart('Welcome to the token-hts-hscs-interop task!');
+
+    // Read in environment variables from `.env` file in parent directory
+    dotenv.config({ path: '../.env' });
+    logger.log('Read .env file');
+
+    // Initialise the operator account
+    const operatorIdStr = process.env.OPERATOR_ACCOUNT_ID;
+    const operatorKeyStr = process.env.OPERATOR_ACCOUNT_PRIVATE_KEY;
+    if (!operatorIdStr || !operatorKeyStr) {
+        throw new Error('Must set OPERATOR_ACCOUNT_ID and OPERATOR_ACCOUNT_PRIVATE_KEY environment variables');
+    }
+    const operatorId = AccountId.fromString(operatorIdStr);
+    const operatorKey = PrivateKey.fromStringECDSA(operatorKeyStr);
+    client = Client.forTestnet().setOperator(operatorId, operatorKey);
+    logger.log('Using account:', operatorIdStr);
+
+    await logger.logSectionWithWaitPrompt('Running the main part of the script');
+    console.log('Doing something that takes 1 second.');
+    await (new Promise((resolve) => { setTimeout(resolve, 1_000) }));
+    if (!!true) {
+        throw new Error('Demo error, this was inevitable!');
+    }
+
+    client.close();
+    logger.logComplete('Demo task complete!');
+}
+
+scriptToken-hts-hscs-interop().catch((ex) => {
+    client && client.close();
+    logger ? logger.logError(ex) : console.error(ex);
+});
